849cfbf2c55b6ccd2d0baa40b9dfb7715a2b052c
mohit mishra <mrmohitmishra@gmail.com>
2020-10-04 02:03:09 +0530

    added blog

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,5 @@ class Blog(models.Model):
     title = models.CharField(max_length = 100)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    summary = models.CharField(max_length = 300)
     image = models.ImageField(upload_to = 'images/')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 def blog(request):
-    return render(request,'blog/blog.html',{})
+    blogposts = Blog.objects
+    return render(request,'blog/blog.html',{'blogposts':blogposts})
