67920f8bfadca030ac681ecde12e16be76bef7cf
mrmohitmishr <mrmohitmishra@gmail.com>
2020-10-18 15:16:33 +0000

    ok

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-r_s&zrdnvi*z=fe4+&lkuduc9dah*9kj5-j5say7z_4ux-g)%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['157.245.106.189','mohitmishra.online','www.mohitmishra.online','localhost','127.0.0.1']
 
